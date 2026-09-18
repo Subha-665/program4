52224f6062a2f78c9d0c4ab3f7f8bdf6077e0b66
Subha-665 <subhapratha015@gmail.com>
2026-09-18 13:30:23 +0530

    Update student_solution.sh

--- student_solution.sh
+++ student_solution.sh
@@ -5,29 +5,32 @@
 # Register Number:
 
 echo "===== SELinux Status ====="
-
+getenforce
 
 echo "===== Creating Web Directory ====="
-
+mkdir -p /webdata
 echo "===== Creating HTML File ====="
-
+echo "Hello SELinux" > /myweb/index.html
 
 echo "===== Setting Linux Permissions ====="
-
+chmod 755 /myweb
+chmod 644 /myweb/index.html
 
 echo "===== Checking Initial Context ====="
-
+ls -Zd /myweb
+ls -Z /myweb/index.html
 
 echo "===== Assigning Wrong SELinux Context ====="
+chcon -t default t /myweb/index.html
 
 echo "===== Checking Wrong Context ====="
-
+ls -Z /myweb/index.html
 
 echo "===== Checking AVC Denials ====="
 echo "===== Correcting SELinux Context ====="
-
+ausearch -m AVC,USER_AVC -ts recent
 
 echo "===== Checking Correct Context ====="
 
-
+sealert -a /var/log/audit/audit.log
 echo "===== Practical Completed ====="
